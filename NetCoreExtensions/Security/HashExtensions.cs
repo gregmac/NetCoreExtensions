fd0e6d4364736cc8bb03751df030e18acd73d629
Greg MacLellan <greg@gregmaclellan.com>
2019-01-05 17:22:19 -0500

    Fix XML comments on hash functions for new output type

--- NetCoreExtensions/Security/HashExtensions.cs
+++ NetCoreExtensions/Security/HashExtensions.cs
@@ -10,27 +10,24 @@ namespace NetCoreExtensions.Security
     public static class HashExtensions
     {
         /// <summary>
-        /// Compute a SHA1 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA1 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test, assumed to be UTF-8.</param>
+        /// <param name="input">The input string to hash, assumed to be UTF-8.</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha1(this string input)
             => Sha1(input, Encoding.UTF8);
 
         /// <summary>
-        /// Compute a SHA1 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA1 hash for the input data.
         /// </summary>
-        /// <param name="input">The input string to test</param>
+        /// <param name="input">The input string to hash</param>
         /// <param name="encoding">The encoding format of the input</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha1(this string input, Encoding encoding)
             => Sha1(encoding.GetBytes(input));
 
         /// <summary>
-        /// Compute a SHA1 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA1 hash for the input data. 
         /// </summary>
         /// <param name="input">The raw input data to hash</param>
         /// <returns>Unix format hex string of the hash</returns>
@@ -43,27 +40,24 @@ namespace NetCoreExtensions.Security
         }
 
         /// <summary>
-        /// Compute a SHA256 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA256 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test, assumed to be UTF-8.</param>
+        /// <param name="input">The input string to hash, assumed to be UTF-8.</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha256(this string input)
             => Sha256(input, Encoding.UTF8);
 
         /// <summary>
-        /// Compute a SHA256 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA256 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test</param>
+        /// <param name="input">The input string to hash</param>
         /// <param name="encoding">The encoding format of the input</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha256(this string input, Encoding encoding)
             => Sha256(encoding.GetBytes(input));
 
         /// <summary>
-        /// Compute a SHA256 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA256 hash for the input data.
         /// </summary>
         /// <param name="input">The raw input data to hash</param>
         /// <returns>Unix format hex string of the hash</returns>
@@ -76,27 +70,24 @@ namespace NetCoreExtensions.Security
         }
 
         /// <summary>
-        /// Compute a SHA384 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA384 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test, assumed to be UTF-8.</param>
+        /// <param name="input">The input string to hash, assumed to be UTF-8.</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha384(this string input)
             => Sha384(input, Encoding.UTF8);
 
         /// <summary>
-        /// Compute a SHA384 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA384 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test</param>
+        /// <param name="input">The input string to hash</param>
         /// <param name="encoding">The encoding format of the input</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha384(this string input, Encoding encoding)
             => Sha384(encoding.GetBytes(input));
 
         /// <summary>
-        /// Compute a SHA384 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA384 hash for the input data. 
         /// </summary>
         /// <param name="input">The raw input data to hash</param>
         /// <returns>Unix format hex string of the hash</returns>
@@ -109,27 +100,24 @@ namespace NetCoreExtensions.Security
         }
 
         /// <summary>
-        /// Compute a SHA512 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA512 hash for the input data. 
         /// </summary>
-        /// <param name="input">The input string to test, assumed to be UTF-8.</param>
+        /// <param name="input">The input string to hash, assumed to be UTF-8.</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha512(this string input)
             => Sha512(input, Encoding.UTF8);
 
         /// <summary>
-        /// Compute a SHA512 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA512 hash for the input data.
         /// </summary>
-        /// <param name="input">The input string to test</param>
+        /// <param name="input">The input string to hash</param>
         /// <param name="encoding">The encoding format of the input</param>
         /// <returns>Unix format hex string of the hash</returns>
         public static HashResult Sha512(this string input, Encoding encoding)
             => Sha512(encoding.GetBytes(input));
 
         /// <summary>
-        /// Compute a SHA512 hash for the input data. Output is a string in unix format
-        /// (all hex, lowercase, no special characters).
+        /// Compute a SHA512 hash for the input data.
         /// </summary>
         /// <param name="input">The raw input data to hash</param>
         /// <returns>Unix format hex string of the hash</returns>
